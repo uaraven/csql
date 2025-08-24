aaa5be38c0200cf5f73a8e8e9e0ecfe24dffa878
Les Voronin <ovoronin@gmail.com>
2025-08-24 09:45:49 -0400

    Removed `--no-cache` option and disabled caching, until bugs are fixed

--- main.go
+++ main.go
@@ -28,14 +28,14 @@ import (
 	"github.com/uaraven/csql/core"
 )
 
-var Version = "v0.4.3"
+var Version = "v0.4.4"
 
 var opts struct {
 	NullValue *string `short:"n" long:"null" description:"Sets the string value to be treated as NULL"`
 	RunQuery  bool    `short:"c" long:"command" description:"Runs the query passed as command line arguments"`
 	Separator *string `short:"s" long:"separator" description:"CSV separator character"`
 	Output    *string `short:"o" long:"output" description:"Output results into a file"`
-	NoCache   bool    `long:"no-cache" description:"Disable datasource caching" default:"true"`
+	// NoCache   bool    `long:"no-cache" description:"Disable datasource caching" default:"true"`
 }
 
 func main() {
@@ -52,7 +52,8 @@ func main() {
 			log.Fatalf("Failed to set CSV separator %v", err)
 		}
 	}
-	core.CacheEnabled = !(opts.RunQuery || opts.NoCache)
+	// core.CacheEnabled = !(opts.RunQuery || opts.NoCache)
+	core.CacheEnabled = false
 	if opts.RunQuery {
 		query := strings.Join(args, " ")
 		if opts.Output == nil {
